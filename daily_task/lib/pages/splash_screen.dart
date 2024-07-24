a32981344c7e7d998bb9c17882d89bab9e707b6c
Steffano99 <101764484+Steffano99@users.noreply.github.com>
2024-07-24 01:57:58 +0000

    update

--- daily_task/lib/pages/splash_screen.dart
+++ daily_task/lib/pages/splash_screen.dart
@@ -10,7 +10,7 @@ class Splashscreen extends StatelessWidget {
     return AnimatedSplashScreen(
       splash: Image.asset('assets/images/daily_task.png'),
       nextScreen: const HomePage(),
-      duration: 3000,
+      duration: 2000,
       splashTransition: SplashTransition.fadeTransition,
     );
   }
